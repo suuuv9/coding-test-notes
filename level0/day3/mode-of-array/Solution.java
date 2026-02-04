ec6b4eb270a72b770f51afcaf2b8f91eea60d961
suuuv9 <asb040923@gmail.com>
2026-02-04 11:44:17 +0900

    Update Solution.java

--- level0/day3/mode-of-array/Solution.java
+++ level0/day3/mode-of-array/Solution.java
@@ -1,1 +1,28 @@
+/ 프로그래머스 < 코딩테스트 입문 - Day3 < 최빈값 구하기
 
+  class Solution {
+    public int solution(int[] array) {
+
+        int[] count = new int[1000]; //숫자 등장 횟수 
+
+        //빈도 계산
+        for (int i = 0; i < array.length; i++) {
+            count[array[i]]++;
+        }
+
+        int maxCount = 0; //최대 등장 횟수 
+        int answer = -1; //최대 횟수를 가진 숫자
+
+        //최빈값 찾기
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > maxCount) {
+                maxCount = count[i];
+                answer = i;
+            } else if (count[i] == maxCount && maxCount != 0) {
+                answer = -1; // 최빈값이 여러 개
+            }
+        }
+
+        return answer;
+    }
+}
